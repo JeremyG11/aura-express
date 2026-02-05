2d2f1f510438efa8548cdc29456092e9462142b2
JeremyG11 <gatwech3211@gmail.com>
2026-02-05 09:15:15 +0300

    feat: schema syncing

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/server.ts"],
   format: ["cjs", "esm"],
-  dts: false,
+  dts: true,
   splitting: false,
   sourcemap: true,
   clean: true,
